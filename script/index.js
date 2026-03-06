cea04f00dd8280512eaf4de4b4bb9c193475cb76
amina-islam-meem <amina.meem28@gmail.com>
2026-03-06 14:29:21 +0600

    voice btn

--- script/index.js
+++ script/index.js
@@ -3,6 +3,23 @@ const createElements = (arr) => {
     return createHtml.join(" ")
 }
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
+const managespinner=(status)=>{
+    if(status== true){
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("word-container").classList.add("hidden");
+    }
+    else{
+          document.getElementById("spinner").classList.add("hidden");
+          document.getElementById("word-container").classList.remove("hidden");
+    }
+}
+
 const loadLessons=()=>{
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then(res => res.json())
@@ -48,6 +65,7 @@ const displayWordDetails=(word)=>{
 
 
 const loadLevelWord=(id)=>{
+    managespinner(true);
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url).then(res => res.json())
     .then((data) =>{
@@ -71,6 +89,8 @@ const displayLevelWord=(words)=>{
           <h2 class="text-4xl font-bold font-bangla">নেক্সট Lesson এ যান</h2>
       </div>
         `;
+
+        managespinner(false);
         return;
     }
 
@@ -85,12 +105,13 @@ const displayLevelWord=(words)=>{
 
         <div class="flex justify-between items-center">
           <button onclick="loadWordDetails(${word.id})" class="btn bg-[#1A91FF20] hover:bg-[#1A91FF60]"><i class="fa-solid fa-circle-info"></i></button>
-          <button class="btn bg-[#1A91FF20] hover:bg-[#1A91FF60]"><i class="fa-solid fa-volume-high"></i></button>
+          <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF20] hover:bg-[#1A91FF60]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
         `;
         wordContainer.append(card);
     }
+    managespinner(false);
 }
 
 const displayLesson = (lessons) =>{
@@ -101,7 +122,7 @@ const displayLesson = (lessons) =>{
     {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML= `<button id= "lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})"
-         class="btn btn-outline btn-primary">
+         class="btn btn-outline btn-primary lesson-btn">
         <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
         </button>
         `
